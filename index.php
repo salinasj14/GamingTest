077b59b2de35896c7df36b3517604ae7655ca8e2
josesalinas <jose.salinas88@gmail.com>
2017-02-09 00:08:20 -0500

    check

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ while( $row = sqlsrv_fetch_array( $getProducts, SQLSRV_FETCH_ASSOC ))
 {
     echo "<br>";
     echo $row['Id'].",   ".$row['Name'].",   ".$row['Kills'].",   "
-        .$row['Deaths'].", ".$row['Scores'].", ".$row['Team']."<br />";
+        .$row['Deaths'].",    ".$row['Scores'].",   ".$row['Team']."<br />";
     echo "<br>";
 }
 //}
