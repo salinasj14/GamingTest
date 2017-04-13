7e7302a19dce589e0d8e184fb3323b55a5c1e4ab
josesalinas <jose.salinas88@gmail.com>
2017-04-13 18:56:55 -0400

    check

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $create = sqlsrv_query($conn, $createCmd);
 if($tableOperation == "showData")
 {
    // echo "the name is '$tableName'";
-    $tsql = "SELECT * FROM [dbo].[$tableName]";
+    $tsql = "SELECT * FROM $tableName ORDER BY Scores";//earl
     $getProducts = sqlsrv_query($conn, $tsql);
     if ($getProducts == FALSE)
     {
@@ -68,7 +68,7 @@ if($tableOperation == "makePlayer")
 {
     echo "it works";
     //it should auto increment and have a null value for team.
-    $makeCmd = "INSERT into [dbo].[$tableName] values ('$name',0,0,0,0,0)";
+    $makeCmd = "INSERT into $tableName values ('$name',0,0,0,0,0)";
     $makePlayer = sqlsrv_query($conn, $makeCmd);
 }
 
@@ -78,7 +78,7 @@ if($tableOperation == "deletePlayer")
     echo "about to check delete player";
     echo "<br>";
     echo "you have called table operation (deletePlayer)";
-    $deletePlayerCmd = "DELETE from [dbo].[$tableName] where name = '$name'";
+    $deletePlayerCmd = "DELETE from $tableName where name = '$name'";
     $deletePlayer = sqlsrv_query($conn, $deletePlayerCmd);
     echo "you have finished calling table operation (deletePlayer) \n";
 }
@@ -86,7 +86,7 @@ if($tableOperation == "deletePlayer")
 if($tableOperation == "updateKill")
 {
     echo "you have called table operation (updateKill)";
-    $killCmd = "UPDATE [dbo].[$tableName] set Kills = Kills+1 where Name = '$name'";
+    $killCmd = "UPDATE $tableName set Kills = Kills+1 where Name = '$name'";
     $updateKill = sqlsrv_query($conn,$killCmd);
     echo "you have finished calling  table operation (updatingKill)";
 }
@@ -94,7 +94,7 @@ if($tableOperation == "updateKill")
 if($tableOperation == "updateDeath")
 {
     echo "you have called table operation (updateDeath)";
-    $deathCmd = "UPDATE [dbo].[$tableName] set Deaths = Deaths+1 where Name = '$name'";
+    $deathCmd = "UPDATE $tableName set Deaths = Deaths+1 where Name = '$name'";
     $updateDeath = sqlsrv_query($conn,$deathCmd);
     echo "you have finished calling  table operation (updatingDeath)";
 }
@@ -104,7 +104,7 @@ if($tableOperation == "updateScore")
 {
     echo "you have called table operation (updateScore)";
     echo "<br>";
-    $updateCmd = "UPDATE [dbo].[$tableName] set Scores = Scores+ $score where Name = '$name'";
+    $updateCmd = "UPDATE $tableName set Scores = Scores+ $score where Name = '$name'";
     $updateScore = sqlsrv_query($conn,$updateCmd);
     echo "you have finished calling table operation (updateScore)";
 }
@@ -113,7 +113,7 @@ if($tableOperation == "setScore")
 {
     echo "you have called table operation (setScore)";
     echo "<br>";
-    $setScoreCmd = "UPDATE [dbo].[$tableName] set Scores = $score where Name = '$name'";
+    $setScoreCmd = "UPDATE $tableName set Scores = $score where Name = '$name'";
     $setScore = sqlsrv_query($conn,$setScoreCmd);
     echo "you have finished calling table operation (setScore)";
 }
@@ -122,7 +122,7 @@ if($tableOperation == "setScore")
 if($tableOperation == "incScores")
 {
     echo "you have called table operation (incScores)";
-    $incCmd = "UPDATE [dbo].[$tableName] set Scores = Scores+1 where Name = '$name'";
+    $incCmd = "UPDATE $tableName set Scores = Scores+1 where Name = '$name'";
     $incScores = sqlsrv_query($conn,$incCmd);
     echo "you have finished calling  table operation (incScores)";
 }
@@ -134,13 +134,13 @@ if($tableOperation == "setTeam")
     if($team == 1)
     {
         echo "you entered in setTeam 2!!!";
-        $set = "UPDATE [dbo].[$tableName] set Team = 1 where Name = '$name'";
+        $set = "UPDATE $tableName set Team = 1 where Name = '$name'";
         echo "<br>";
     }
     else if($team == 2)
     {
         echo "you entered in setTeam 2!!!";
-        $set = "UPDATE [dbo].[$tableName] set Team = 2 where Name = '$name'";
+        $set = "UPDATE $tableName set Team = 2 where Name = '$name'";
         echo "<br>";
     }
     $setTeam = sqlsrv_query($conn,$set);
@@ -170,6 +170,5 @@ if($tableOperation == "highestScore")
     while( $row = sqlsrv_fetch_array( $getScore, SQLSRV_FETCH_ASSOC ))
     {
         echo $row['Name']."|".$row['Scores']."|".";";
-        echo "<br>";
     }
 }
