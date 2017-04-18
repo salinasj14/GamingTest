e10546345b4e5158cf3f7befdac4f325fc6d5ae2
josesalinas <jose.salinas88@gmail.com>
2017-04-17 22:09:27 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -171,6 +171,7 @@ if($tableOperation == "gameRefresh")
 //largest round//Earl
 if($tableOperation == "largestRound")
 {
+    echo "you have called largest round";
     $maxRound = "SELECT Name, Round FROM $tableName WHERE Round = (Select max(Round) From $tableName)";
     $getRound = sqlsrv_query($conn, $maxRound);
     while( $row = sqlsrv_fetch_array( $getRound, SQLSRV_FETCH_ASSOC ))
