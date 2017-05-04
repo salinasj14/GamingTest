c31eac8e2ddf0852fb0447b233e116171cfcb909
Jose Salinas <salinasj14@students.ecu.edu>
2017-05-04 11:32:30 -0400

    Add files via upload

---
+++ collection.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using UnityEngine;
+
+public class collection : MonoBehaviour
+{  
+     void OnTriggerEnter2D(BoxCollider2D coin)
+    {
+        if (coin.tag == "Coin") //when going over item with tag coin, it is destroyed.
+        {
+            Destroy(coin.gameObject);
+            
+        }
+    }
+}
